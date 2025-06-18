9afa3a154a735ba27715e9de9933cb9d67397af6
Matheus Rende <matheusrende@hotmail.com>
2025-06-18 14:31:24 -0300

    Create venv
    Apenas criando um ambiente virtual para teste

--- TradutorV2.py
+++ TradutorV2.py
@@ -6,17 +6,20 @@ idiomas = {
     "Ingles": "en",
     "Japones": "ja",
     "Italiano": "it",
-    "Portugues":"pt"
+    "Portugues": "pt"
 }
 
-# Entrada do idioma com tratamento de espaços e case-insensitive
-idioma_escolhido = input('Informe para qual língua deseja traduzir: ').strip().title()
-
-# Verifica se o idioma está no dicionário
-while(True):
+# Loop até que o idioma escolhido seja válido
+while True:
+    idioma_escolhido = input('Informe para qual língua deseja traduzir: ').strip().title()
+    
     if idioma_escolhido in idiomas:
         texto_traduzir = input('O que deseja traduzir? ')
         codigo_idioma = idiomas[idioma_escolhido]  # Obtém o código do idioma
-    # Realiza a tradução
-    traducao = GoogleTranslator(source='auto', target=codigo_idioma).translate(texto_traduzir)
-    print("A tradução ficou:", traducao)
+        
+        # Realiza a tradução
+        traducao = GoogleTranslator(source='auto', target=codigo_idioma).translate(texto_traduzir)
+        print('A tradução ficou:', traducao)
+        break  # Encerra o loop após tradução
+    else:
+        print("Idioma inválido. Tente novamente.")
